cfb9a98014ad41ac57f004fbfc777a38cc0ba7b4
Ali Emadi <ali7ev@gmail.com>
2020-10-28 15:45:03 +0330

    added nodejs server

---
+++ backend/nodejs/server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const crypto = require("crypto");
+const fs = require("fs")
+const port = 3000;
+
+app.use(cors())
+
+app.use(express.json())
+
+app.post("/node/sha256", (req, res) => {
+  let num1 = req.body.num1;
+  let num2 = req.body.num2;
+  if (typeof num1 == "undefined" || typeof num2 == "undefined"){
+    return res.status(400).json({message: "inputs can't be empty"})
+  }
+  if (!(typeof num1 == "number") || !(typeof num2 == "number")){
+    return res.status(400).json({message: "inputs must be numbers"})
+  }
+  const hash = crypto.createHash('sha256').update((num1+num2).toString()).digest('base64');
+  res.json({
+    result: hash,
+  });
+});
+
+app.get("/node/write", (req, res) => {
+  let line = req.query.line;
+  if (typeof line == "undefined"){
+    return res.status(400).json({message: "input can't be empty"})
+  }
+  if (isNaN(line)){
+    return res.status(400).json({message: "input must be a number"})
+  }
+  if (line < 0 || line > 100){
+    return res.status(400).json({message: "line number must be between 0 and 100"})
+  }
+  file = fs.readFile("./tesdsdst.txt", function (err,data) {
+    if (err) return res.status(500).json({message: "internal server error"});
+    let lines = data.toString().split(/[\r\n]+/);
+    res.json({
+      result: lines[line],
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`HW1 app listening at http://localhost:${port}`);
+});
